2cc4ac0968a32a08ba3ccfcf7abaa3cc8d488f1a
Tristan Gray-Davies <tgraydavies@gmail.com>
2022-12-02 00:08:15 +0000

    new implementation which only keeps top 3 calories in memory, and more elegantly handles the end of the file

--- Day01/day01.jl
+++ Day01/day01.jl
@@ -1,16 +1,26 @@
 using DataStructures
 
+K = 3
+
 function day01(file)
-    h = MutableBinaryMaxHeap{Int}();
-    thiscal = 0;
-    for l in readlines(file)
-        if isempty(l)
-            push!(h,thiscal);
-            thiscal = 0;
-        else
-            thiscal += parse(Int,l);
+    topk = BinaryMinMaxHeap{Int}(zeros(Int,K));
+    while !eof(file)
+        cal = getnextelfcalories(file);
+        if cal>minimum(topk)
+            popmin!(topk);
+            push!(topk,cal);
         end
     end
-    push!(h,thiscal); # last elf
-    return first(h),sum(pop!(h) for _ in 1:3)
+    return maximum(topk),sum(popall!(topk))
+end
+
+function getnextelfcalories(file)
+    cal = 0;
+    while true
+        line = readline(file); # returns empty at end of file
+        if isempty(line)
+            return cal
+        end
+        cal += parse(Int,line);
+    end
 end
