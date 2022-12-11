61f017b82f64b5068fe30683c96dc4b6e389178d
Tristan Gray-Davies <tgraydavies@gmail.com>
2022-12-11 19:07:39 +0000

    tidy

--- Day11/day11.jl
+++ Day11/day11.jl
@@ -9,16 +9,17 @@ mutable struct Monkey
     function Monkey(text)
         items = [parse(Int,m.match) for m in eachmatch(r"([0-9]+)",text[2])];
         divby = parse(Int,match(r"([0-9]+)",text[4]).match);
-        monkeyto = Tuple(parse(Int,match(r"([0-9]+)",line).match) for line in text[5:6]);
+        monkeyto = Tuple(1+parse(Int,match(r"([0-9]+)",line).match) for line in text[5:6]); # 1+ to move to 1-indexing
         op,arg = match(r"old (\+|\-|\*|\+) (old|[0-9]+)",text[3]).captures;
         if     op=="+" f = +;
         elseif op=="-" f = -;
         elseif op=="*" f = *;
         end
-        if arg=="old";
+        if arg=="old"
             func = x->f(x,x);
         else
-            func = x->f(x,parse(Int,arg));
+            v = parse(Int,arg);
+            func = x->f(x,v);
         end
         itemsq = Deque{Int}();
         for i in items
@@ -30,34 +31,34 @@ end
 
 function day11(file)
     latestmonkey = Iterators.takewhile(!isempty,eachline(file));
-    monkeys = []; # ::Vector{Monkey}
+    monkeys::Vector{Monkey} = [];
     while !eof(file)
-        push!(monkeys,Monkey([line for line in latestmonkey]));
+        push!(monkeys,Monkey(collect(latestmonkey)));
     end
-    commondemom = prod(m.testdivisibleby for m in monkeys)
+    lcm = prod(m.testdivisibleby for m in monkeys)
     monkeyspart2 = deepcopy(monkeys);
     for _ in 1:20, i = 1:length(monkeys)
-        taketurn!(monkeys[i],monkeys,true,commondemom);
+        taketurn!(monkeys[i],monkeys,true,lcm);
     end
     for _ in 1:10000, i = 1:length(monkeyspart2)
-        taketurn!(monkeyspart2[i],monkeyspart2,false,commondemom);
+        taketurn!(monkeyspart2[i],monkeyspart2,false,lcm);
     end
     inspections = sort([m.ninspections for m in monkeys],rev=true);
     inspections2 = sort([m.ninspections for m in monkeyspart2],rev=true);
     return prod(inspections[1:2]),prod(inspections2[1:2])
 end
 
-function taketurn!(m::Monkey,monkeys,ispart1,commondemom)
+function taketurn!(m::Monkey,monkeys::Vector{Monkey},ispart1::Bool,lcm::Int)
     while !isempty(m.items)
         x = popfirst!(m.items);
         x = m.operation(x);
         if ispart1
             x ÷= 3;
         else
-            x = mod(x,commondemom);
+            x = mod(x,lcm);
         end
         indto =  x % m.testdivisibleby ==0 ? 1 : 2;
-        push!(monkeys[m.monkeyto[indto]+1].items,x);
+        push!(monkeys[m.monkeyto[indto]].items,x);
         m.ninspections += 1;
     end
 end
